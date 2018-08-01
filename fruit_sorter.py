4550450602f07e63ab2727ece60ddfa4527e2044
dima20-meet <dima20@meet.mit.edu>
2018-08-01 05:26:42 -0400

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,13 @@
+new_fruit=input('what fruit am I sorting?')
+
+if new_fruit == Apple:
+    print ('Bin 1')
+
+elif new_fruit == Orange:
+    print ('Bin 2')
+
+elif new_fruit == Olive:
+    print ('Bin 3')
+
+else:
+    print('Error! I do not recognize this fruit')
---
+++ turtle_party.py
@@ -0,0 +1,10 @@
+strawberries = 20
+is_weekend = True
+
+if (strawberries > 39 and strawberries < 61 and not is_weekend):
+    print ('Fun!')
+
+elif (strawberries > 39 and is_weekend):
+    print ('Fun!')
+else:     
+    print('Not Fun!')
